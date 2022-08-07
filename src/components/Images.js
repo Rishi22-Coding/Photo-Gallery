bdd1024ab60ef4b2dd91a0020c89687534a75296
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-08-07 17:28:58 +0530

    Final Styling

--- src/components/Images.js
+++ src/components/Images.js
@@ -8,7 +8,7 @@ const Images = () => {
   return (
     <>
         <h1
-        className="text-center mt-6 underline text-2xl"
+        className="text-center mt-6 underline text-2xl "
         >Results for {searchImage || 'Computer'}</h1>
         <div 
         className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 max-auto px-4 my-7">

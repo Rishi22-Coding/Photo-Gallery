1936b1077f1feec1f3da35e4c1d90fc82c0704e6
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-08-07 15:36:25 +0530

    Axios Added for API Calling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
+import Images from './components/Images';
+import Axios from './components/Axios';
 
 function App() {
+  const { response, isLoading, error, fetchData } = Axios(`search/photos?page=1&query=office&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+  console.log(response);
   return (
     <>
       <NavBar >
         <SearchBar />
       </NavBar>
+      <Images />
     </>
   );
 }
